e37d73cc492ea3750d67ec246f220f2eee09dddc
Bruno Santos <bruno403f@gmail.com>
2017-07-09 01:18:01 -0400

    initial works on client and server code with tp1opt library

--- client.c
+++ client.c
@@ -1,11 +1,22 @@
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
 #include "tp1opt.h"
-
 
 int main(int argc, char** argv) {
 
-    struct netconfigs options;              // Struct for store program's configs
+    int sockfd;
+    char* retvalue;
+    char buffer[256];
+
+    struct hostent *server;
+    struct sockaddr_in serv_addr;
+    struct netconfigs options;      // Struct for store program's configs
 
     // Get all configs by user
     if (set_options(argc, argv, 0, &options) < 0) {
@@ -13,5 +24,51 @@ int main(int argc, char** argv) {
         exit(EXIT_FAILURE);
     }
 
-    return EXIT_SUCCESS;
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) {
+        fprintf(stderr, "ERROR: %s\n", strerror(errno));
+        exit(1);
+    }
+
+    server = gethostbyname(options.ip_address);
+    if (server == NULL) {
+        fprintf(stderr, "ERROR, no such host\n");
+        exit(1);
+    }
+
+    memset((char*) &serv_addr, 0, sizeof(serv_addr)); // Zero the struct
+    memcpy((char*) &serv_addr.sin_addr.s_addr, server->h_addr,
+           (size_t) server->h_length);
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons((uint16_t) options.connection_port);
+
+    if (connect(sockfd, (struct sockaddr*) &serv_addr, sizeof(serv_addr)) < 0) {
+        fprintf(stderr, "ERROR: %s\n", strerror(errno));
+        exit(1);
+    }
+
+    printf("Please enter the message: ");
+    memset(buffer, 0, sizeof(buffer));
+
+    do {
+        retvalue = fgets(buffer, sizeof(buffer), stdin);
+    } while (retvalue == NULL);
+
+    if (write(sockfd, buffer, sizeof(buffer)) < 0) {
+        fprintf(stderr, "ERROR: %s\n", strerror(errno));
+        exit(1);
+    }
+
+    memset(buffer, 0, sizeof(buffer));
+    if (read(sockfd, buffer, sizeof(buffer) - 1) < 0) {
+        fprintf(stderr, "ERROR: %s\n", strerror(errno));
+        exit(1);
+    }
+
+    printf("%s\n", buffer);
+
+    close(sockfd);
+
+    return 0;
 }
--- server.c
+++ server.c
@@ -1,11 +1,24 @@
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 #include "tp1opt.h"
 
+#define LISTEN_ENQ 5
 
 int main(int argc, char** argv) {
 
-    struct netconfigs options;              // Struct for store program's configs
+    int sockfd;
+    int newsockfd;
+    int clilen;
+    char buffer[256];
+
+    struct sockaddr_in serv_addr;
+    struct sockaddr_in cli_addr;
+    struct netconfigs options;      // Struct for store program's configs
 
     // Get all configs by user
     if (set_options(argc, argv, 1, &options) < 0) {
@@ -13,5 +26,47 @@ int main(int argc, char** argv) {
         exit(EXIT_FAILURE);
     }
 
-    return EXIT_SUCCESS;
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) {
+        fprintf(stderr, "ERROR: %s\n", strerror(errno));
+        exit(1);
+    }
+
+    memset((char*) &serv_addr, 0, sizeof(serv_addr)); // Zero the struct
+
+    //portno = atoi(argv[1]);
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons((uint16_t) options.connection_port);
+
+    if (bind(sockfd, (struct sockaddr*) &serv_addr, sizeof(serv_addr)) < 0) {
+        fprintf(stderr, "ERROR: %s\n", strerror(errno));
+        exit(1);
+    }
+
+    listen(sockfd, LISTEN_ENQ);
+    clilen = sizeof(cli_addr);
+    newsockfd = accept(sockfd, (struct sockaddr*) &cli_addr, (unsigned int*) &clilen);
+    if (newsockfd < 0) {
+        fprintf(stderr, "ERROR: %s\n", strerror(errno));
+        exit(1);
+    }
+    memset(buffer, 0, sizeof(buffer));
+
+    if (read(newsockfd, buffer, sizeof(buffer)) < 0) {
+        fprintf(stderr, "ERROR: %s\n", strerror(errno));
+        exit(1);
+    }
+
+    printf("Here is the message: %s", buffer);
+
+    if (write(newsockfd, "Recebi a mensagem!", 18) < 0) {
+        fprintf(stderr, "ERROR: %s\n", strerror(errno));
+        exit(1);
+    }
+
+    close(newsockfd);
+    close(sockfd);
+
+    return 0;
 }
