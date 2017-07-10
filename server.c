38f86d006e502006579e11d5b0bc37033421a473
Bruno Santos <bruno403f@gmail.com>
2017-07-09 20:44:20 -0400

    first implementations on fork / multithreading

--- server.c
+++ server.c
@@ -9,23 +9,20 @@
 #include "tp1opt.h"
 
 #define LISTEN_ENQ 5
+#define BUFFER_MAX_SIZE 256
 
 
 int sockfd, newsockfd;
 
 
-void sighandler(int signum) {
-    printf("CTRL+C pressed\n");
-    close(newsockfd);
-    close(sockfd);
-    exit(0);
-}
+void sighandler(int signum);
 
+void* communication_handler(void* arg);
 
 int main(int argc, char** argv) {
 
     int clilen;
-    char buffer[256];
+    pid_t pid;
 
     struct sockaddr_in serv_addr;
     struct sockaddr_in cli_addr;
@@ -46,8 +43,6 @@ int main(int argc, char** argv) {
     }
 
     memset((char*) &serv_addr, 0, sizeof(serv_addr)); // Zero the struct
-
-    //portno = atoi(argv[1]);
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = INADDR_ANY;
     serv_addr.sin_port = htons((uint16_t) options.connection_port);
@@ -59,27 +54,53 @@ int main(int argc, char** argv) {
 
     listen(sockfd, LISTEN_ENQ);
     clilen = sizeof(cli_addr);
-    newsockfd = accept(sockfd, (struct sockaddr*) &cli_addr, (unsigned int*) &clilen);
-    if (newsockfd < 0) {
-        fprintf(stderr, "ERROR: %s\n", strerror(errno));
-        exit(1);
+
+    while (1) {
+        newsockfd = accept(sockfd, (struct sockaddr*) &cli_addr, (unsigned int*) &clilen);
+        if (newsockfd < 0) {
+            fprintf(stderr, "ERROR: %s\n", strerror(errno));
+            exit(1);
+        }
+        pid = fork();
+        if (pid < 0) {
+            fprintf(stderr, "ERROR forking process.\n");
+            exit(1);
+        }
+        if (pid == 0) { // Child process
+            communication_handler((void*) &newsockfd);
+            close(sockfd);
+            return 0;
+        } else {
+            close(newsockfd);
+        }
     }
+
+}
+
+
+void* communication_handler(void* arg) {
+
+    int sckt = *((int*) arg);
+    char buffer[BUFFER_MAX_SIZE];
+
     memset(buffer, 0, sizeof(buffer));
-
-    if (read(newsockfd, buffer, sizeof(buffer)) < 0) {
+    if (read(sckt, buffer, sizeof(buffer)) < 0) {
         fprintf(stderr, "ERROR: %s\n", strerror(errno));
         exit(1);
     }
-
     printf("Here is the message: %s", buffer);
-
-    if (write(newsockfd, "Recebi a mensagem!", 18) < 0) {
+    if (write(sckt, "Recebi a mensagem!", 18) < 0) {
         fprintf(stderr, "ERROR: %s\n", strerror(errno));
         exit(1);
     }
+    close(sckt);
+    return NULL;
+}
 
+
+void sighandler(int signum) {
+    printf("CTRL+C pressed\n");
     close(newsockfd);
     close(sockfd);
-
-    return 0;
+    exit(0);
 }
